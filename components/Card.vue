2fca32443d2cae57dd7becc9bf7fa678a5b2ed96
MatheusFr27 <matheusefranca1727@gmail.com>
2020-11-05 15:17:02 -0300

    Adicionando componente Card

---
+++ components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="column">
+    <div class="card">
+      <div class="card-header">
+        <h1 class="card-header-title">{{ title }}</h1>
+      </div>
+      <div class="card-content">{{ description }}</div>
+      <div class="card-footer">
+        <div v-if="author" class="card-footer-item">
+          <p class="text"><strong>Criado por: </strong>{{ author }}</p>
+        </div>
+        <div class="card-footer-item">
+          <p class="text"><strong>Data da postagem: </strong> {{ date }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    description: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    author: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    date: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+}
+</script>
