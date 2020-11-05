d84ed43e8514406e66c4fa0d8c073f02c7e7d589
MatheusFr27 <matheusefranca1727@gmail.com>
2020-11-05 15:22:44 -0300

    Importando componente Card na página posts

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,7 +1,24 @@
 <template>
-  <section class="section">Aqui onde os posts aparecerão!</section>
+  <section class="section mx-0">
+    <h1 class="title has-text-centered">Postagens recentes</h1>
+    <section class="section mx-0">
+      <div class="columns mx-0">
+        <Card
+          title="Um diaaa"
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga eos vero dolor fugiat! Culpa, architecto impedit voluptatibus earum libero optio."
+          author="Matheus"
+          date="05/11/2020"
+        />
+      </div>
+    </section>
+  </section>
 </template>
 
 <script>
-export default {}
+import Card from '../components/Card'
+export default {
+  components: {
+    Card,
+  },
+}
 </script>
