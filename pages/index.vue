1b1fbb755b45831e4beb44f7063a45910a804a4e
MatheusFr27 <matheusefranca1727@gmail.com>
2020-11-05 15:22:00 -0300

    Mudança pequena

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <section class="section">Sou a Página inicial!</section>
+  <section class="section mx-0">
+    <h1 class="title has-text-centered">Seja Bem-vindo!</h1>
+  </section>
 </template>
 
 <script>
 export default {
   name: 'HomePage',
-
-  components: {},
 }
 </script>
